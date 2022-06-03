b02fb87c693ed224f6166bbd845b3abc6851a6c1
erick aguirre <chununo@gmail.com>
2022-06-03 16:24:53 -0500

    app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://nube.narcisse.mx/';
+    public $baseURL = 'http://localhost/narcisse/nube/';
 
     /**
      * --------------------------------------------------------------------------
